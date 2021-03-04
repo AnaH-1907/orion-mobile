5cd7429ff1f8e44a14367e6bf32e71654ea3731e
AnaH-1907 <anaisgagneur@gmail.com>
2021-03-04 10:30:31 +0100

    tab navigation icon

--- components/Shell.jsx
+++ components/Shell.jsx
@@ -3,17 +3,32 @@ import Profile from './Profile';
 import Exams from './Exams';
 import Home from './Home';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
 const Shell = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home}/>
-            <Tab.Screen name="Exams" component={Exams}/>
-            <Tab.Screen name="Profile" component={Profile}/>
+        <Tab.Navigator tabBarOptions={{ showLabel: false }}>
+            <Tab.Screen 
+                name="Home" 
+                component={Home}
+                options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="earth" size={24} color="black" />),
+            }}/>
+            <Tab.Screen 
+                name="Exams" 
+                component={Exams}
+                options={{ tabBarIcon: ({ color, size }) => (<FontAwesome name="check-circle" size={24} color="black" />),
+            }}/>
+            <Tab.Screen 
+                name="Profile" 
+                component={Profile}
+                options={{ tabBarIcon: ({ color, size }) => (<FontAwesome name="user-circle" size={24} color="black" />),
+            }}/>
         </Tab.Navigator>
     )
 };
 
 export default Shell;
+
